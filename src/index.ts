64c4117926f4d9cf7fbafd610b05ec575f8b13ec
kevinquaedvlieg <kevin@kqcreations.com>
2023-01-30 19:19:27 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
+// All errors
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
+export * from './errors/bad-request-error';
 
-const color: Color = {
-    red: 20,
-    blue: 10,
-    green: 10
-}
-
-console.log(color);
-
-export default color;
+// Middlewares
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/validate-request'
